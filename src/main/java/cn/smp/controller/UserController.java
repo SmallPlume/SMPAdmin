1a9cacc6287a24bb1924fb6ab7fa4e8ae8fe4624
fangxiaoyu1900@126.com <fangxiaoyu1900@126.com>
2018-05-29 00:30:47 +0800

    new project

--- src/main/java/cn/smp/controller/UserController.java
+++ src/main/java/cn/smp/controller/UserController.java
@@ -3,16 +3,28 @@ package cn.smp.controller;
 import cn.smp.facade.entity.User;
 import cn.smp.facade.service.UserFacade;
 import cn.smp.support.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/v1/user")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserFacade userFacade;
+
+    @RequestMapping("/add")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        log.info("add request");
+        modelAndView.addObject("email", "123456@abc.com");
+        modelAndView.setViewName("user/add");
+        return modelAndView;
+    }
 
     /**
      * 新增用户
